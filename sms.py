6e8f70902f43dd5708758d1f503432028b67b2f8
maazkhanm9 <157524791+maazkhanm9@users.noreply.github.com>
2024-01-24 00:24:05 +0530

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,36 @@
+import dotenv
+dotenv.load_dotenv()
+
+import requests
+import os
+from twilio.rest import Client
+import time
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = 'accountSID'
+auth_token = 'authtoken'
+client = Client(account_sid, auth_token)
+
+print(auth_token, account_sid)
+
+def getBitcoinPrice():
+    getPrice = requests.get('https://api.bybit.com/spot/v3/public/quote/ticker/price?symbol=BTCUSDT').json()
+    btcPrice = getPrice['result']['price']
+    return btcPrice
+
+print(getBitcoinPrice())
+
+
+
+while True:
+    message = client.messages \
+                    .create(
+                        body=f"BitcoinPrice: {getBitcoinPrice()}",
+                        from_='number given on Twilio',
+                        to='yourPhoneNumber'
+                    )
+
+    print(message.sid)
+    time.sleep(300)
